b5f66ef6ba32a48f5e5c00c9054c9df73dda0c11
JerryChen97 <chenys13@outlook.com>
2020-04-29 16:54:50 -0400

    created the first two rough corr maps

--- psi_generator.py
+++ psi_generator.py
@@ -1,6 +1,3 @@
-""" In this script we will read out the psi for each point from the .h5 fiels in /data folder
-"""
-
 import numpy as np
 from kitaev_ladder import KitaevLadderModel, run_save, read_psi
 
@@ -20,14 +17,8 @@ J_list = [(Jx, Jy, Jz, chi) for Jx in Jx_list for Jy in Jy_list for Jz in Jz_lis
 verbose = 0
 
 if __name__ == "__main__":
-    Jx = 1.  
-    chi = 30 
-    def f(Jy, Jz):
-        psi = read_psi(chi=chi, Jx=Jx, Jy=Jy, Jz=Jz)
-        return psi.correlation_length()
-    
-    ff = np.frompyfunc(f, 2, 1)
-    Jy, Jz = np.meshgrid(Jy_list, Jz_list)
-    
-    corr = ff(Jy, Jz)
-    print(corr)
+    for Jx in Jx_list:
+        for Jy in Jy_list:
+            for Jz in Jz_list:
+                for chi in chi_list:
+                    run_save(chi=chi, Jx=Jx, Jy=Jy, Jz=Jz, verbose=verbose)
