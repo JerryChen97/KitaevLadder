932763353ba99c377b759419553d00a43dc5cb2e
JerryChen97 <chenys13@outlook.com>
2020-05-01 19:06:20 -0400

    more data

--- phase_diagram_spherical.py
+++ phase_diagram_spherical.py
@@ -20,14 +20,27 @@ Nphi = 2 * N + 1
 theta_list = np.linspace(0, pi/2, Ntheta)
 phi_list = np.linspace(0, pi, Nphi)
 
+def get_J(theta, phi):
+    Jx, Jy, Jz = spherical_to_decarte(r=1.0, theta=theta, phi=phi)
+    if Jx == -0.0:
+        Jx = 0.0
+    if Jy == -0.0:
+        Jy = 0.0
+    if Jz == -0.0:
+        Jz = 0.0
+    # if Jx = Jy then slightly differ them to avoid the symmetric state
+    if Jx == Jy:
+        Jx += 0.001
+        Jy -= 0.001
+    return (Jx, Jy, Jz)
+
+# run the DMRG 
 for theta in theta_list:
+    psi = None
     for phi in phi_list:
-        Jx, Jy, Jz = spherical_to_decarte(r=1.0, theta=theta, phi=phi)
-        if Jx == -0.0:
-            Jx = 0.0
-        if Jy == -0.0:
-            Jy = 0.0
-        if Jz == -0.0:
-            Jz = 0.0
-        run_save(Jx=Jx, Jy=Jy, Jz=Jz, verbose=0)
+        Jx, Jy, Jz = get_J(theta, phi)
+        
+        print("\n\n\n Calculating the (Jx, Jy, Jz) = (%.3f, %.3f, %.3f) ground state" % (Jx, Jy, Jz))
+
+        psi = run_save(chi=100, Jx=Jx, Jy=Jy, Jz=Jz, L=2, verbose=1, psi=psi)
         
